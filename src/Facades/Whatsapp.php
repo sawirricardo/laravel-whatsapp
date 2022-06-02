f1f41c0a54e81a6e7aec3524169e5148e60d8b14
sawirricardo <sawir.ricardo@gmail.com>
2022-06-02 10:23:12 +0700

    Update Whatsapp.php

--- src/Facades/Whatsapp.php
+++ src/Facades/Whatsapp.php
@@ -5,6 +5,8 @@ namespace Sawirricardo\Whatsapp\Laravel\Facades;
 use Illuminate\Support\Facades\Facade;
 
 /**
+ * @method static \Sawirricardo\Whatsapp\Whatsapp create ($client = null)
+ * @method static \Sawirricardo\Whatsapp\Webhook webhook ()
  * @see \Sawirricardo\Whatsapp\Whatsapp
  */
 class Whatsapp extends Facade
